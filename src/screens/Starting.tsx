c3c7479c81e6de9858f893e7045c800ffae98c1f
jeffngugi <ngugimwathi254@gmail.com>
2021-11-15 23:22:56 +0300

    feat:Gradient container

--- src/screens/Starting.tsx
+++ src/screens/Starting.tsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native'
 import {COLORS, FONTS, icons, images, SIZES} from '../constants'
+import PrimaryGradient from '../utils/PrimaryGradient'
 
 const Starting = () => {
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.imageContainer}>
-                <Image source={images.start} style={styles.image} resizeMode='cover' />
+                <View style={{width:'100%', height:'100%'}}>
+                    <Image source={images.start} style={styles.image} resizeMode='cover' />
+                    <View style={styles.overlay}/>
+                </View>
                 <View style={styles.yellowBar } />
             </View>                
                 <View  style={styles.startedContainer}>
+                   <PrimaryGradient style={styles.startedContainer}>
                     <View style={{flexDirection:'row', alignSelf:'center', marginVertical:SIZES.padding+2}}>
                         <Text style={styles.my}>My</Text>
                         <Text style={styles.shop}>Shop</Text>
                     </View>
+                    <View style={{marginHorizontal:SIZES.base}}>
                     <Text style={styles.intro}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</Text>
-                    <TouchableOpacity style={styles.btn}>
+                    </View>
+                   
+                    <TouchableOpacity 
+                        onPress={()=>console.log('get to the login screen')}
+                        style={styles.btn}
+                    >
+                        
                         <Text style={styles.btnTxt}>Get Started</Text>
                     </TouchableOpacity>
+                    </PrimaryGradient>
                 </View>
         </SafeAreaView>
     )
@@ -38,11 +51,12 @@ const styles = StyleSheet.create({
         height:'100%',
         width:'100%'
     },
-    otherContainer:{
-        
+    overlay:{
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: '#2A034B75'
     },
     yellowBar:{
-        backgroundColor:'#E7B94495  ',
+        backgroundColor:'#E7B94480',
         height:SIZES.height*(1/8),
         width:SIZES.width*(2/3),
         alignSelf:'flex-end',
@@ -53,24 +67,25 @@ const styles = StyleSheet.create({
     },
     startedContainer:{
         flex:2,
-        backgroundColor:'red',
         borderTopLeftRadius:SIZES.padding,
         borderTopRightRadius:SIZES.padding,
         top:-SIZES.padding,
-        marginBottom:-SIZES.padding
+        marginBottom:-SIZES.padding,
     },
     btn:{
         backgroundColor:COLORS.yellow,
         alignItems:'center',
         alignSelf:'center',
         paddingHorizontal:SIZES.base*2,
+        paddingVertical:SIZES.base/2,
         borderRadius:SIZES.base,
         marginVertical:SIZES.padding
     },
     btnTxt:{
         fontSize:SIZES.h2,
         color:COLORS.white,
-        marginVertical:SIZES.base/2
+        marginVertical:SIZES.base/2,
+        fontWeight:'bold'
     },
     my:{
         color:COLORS.white,
@@ -84,6 +99,10 @@ const styles = StyleSheet.create({
     },
     intro:{
         alignSelf:'center',
-        marginVertical:SIZES.base
+        marginVertical:SIZES.base,
+        textAlign:'center',
+        ...FONTS.body3,
+        margin:SIZES.base,
+        color:COLORS.white2
     }
 })
---
+++ src/utils/PrimaryGradient.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import LinearGradient from 'react-native-linear-gradient';
+
+
+const  GradientStyle = ({ children, style }) => {
+  return (
+      <LinearGradient
+      style={{...style}}
+      colors={['#2A034B', '#691fa4']}
+      start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
+      >
+      {children}
+    </LinearGradient>
+  );
+};
+
+export default GradientStyle
